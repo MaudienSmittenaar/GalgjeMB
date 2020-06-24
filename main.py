5554211d4f5960d42264336b386d202e6c4641c7
Maudien Smittenaar <110417@st-maartenscollege.nl>
2020-06-24 08:06:34 +0000

    Error hersteld.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("We spelen het spel galgje. Leuk dat je meedoet! Raad letters en kom zo achter het woord. Weet het je hele woord? Type dan het woord voluit om te winnen!")
+import re
+import random
 
 woordenlijst = ["school", "corona", "leerwerk", "huiswerk", "vakantie", "zomer", "python","informatica", "winnaar", "computer", "schoolboek", "zon", "zwemmen", "uitrusten"]
 
@@ -6,4 +7,5 @@ hetwoord = random.choice(woordenlijst)
 lengtewoord = len(hetwoord)
 temp = "." * lengtewoord
 
-print("Laten we beginnen! Om je een beetje opweg te helpen vertel ik je hoeveel letters het woord heeft. Het woord heeft" + str(lengtewoord) + " letters") 
+print("We spelen het spel galgje. Leuk dat je meedoet! Raad letters en kom zo achter het woord. Weet het je hele woord? Type dan het woord voluit om te winnen! Laten we beginnen! Om je een beetje opweg te helpen vertel ik je hoeveel letters het woord heeft. Het woord heeft " + str(lengtewoord) + " letters") 
+
