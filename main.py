1d25cdfd73675f4a31182137b4f61ea9293d583d
Maudien Smittenaar <110417@st-maartenscollege.nl>
2020-06-24 08:01:11 +0000

    willekeurigheid voor het gekozen woord toegevoegd.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 print("We spelen het spel galgje. Leuk dat je meedoet! Raad letters en kom zo achter het woord. Weet het je hele woord? Type dan het woord voluit om te winnen!")
 
 woordenlijst = ["school", "corona", "leerwerk", "huiswerk", "vakantie", "zomer", "python","informatica", "winnaar", "computer", "schoolboek", "zon", "zwemmen", "uitrusten"]
+
+hetwoord = random.choice(woordenlijst)
+lengtewoord = len(hetwoord)
+temp = "." * lengtewoord
