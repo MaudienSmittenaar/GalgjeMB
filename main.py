45e0dab94cec14600618e731a4fb578d1573d97f
Maudien Smittenaar <110417@st-maartenscollege.nl>
2020-06-24 08:10:11 +0000

    Reactie als het woord geraden is toegevoegd.

--- main.py
+++ main.py
@@ -7,5 +7,13 @@ hetwoord = random.choice(woordenlijst)
 lengtewoord = len(hetwoord)
 temp = "." * lengtewoord
 
-print("We spelen het spel galgje. Leuk dat je meedoet! Raad letters en kom zo achter het woord. Weet het je hele woord? Type dan het woord voluit om te winnen! Laten we beginnen! Om je een beetje opweg te helpen vertel ik je hoeveel letters het woord heeft. Het woord heeft " + str(lengtewoord) + " letters") 
+print("We spelen het spel galgje. Leuk dat je meedoet! Raad letters en kom zo achter het woord. Weet het je hele woord? Type dan het woord voluit om te winnen! Laten we beginnen!")
+print("Om je een beetje opweg te helpen vertel ik je hoeveel letters het woord heeft!")
+print("Het woord heeft " + str(lengtewoord) + " letters") 
 
+while True:
+  gokje = (input(": "))
+  match = re.search(gokje, hetwoord)
+  if gokje == hetwoord: 
+    print('je heb het woord ' + '"' + hetwoord + '"' + " geraden! Goed gedaan!")
+    break
