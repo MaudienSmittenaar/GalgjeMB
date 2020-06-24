f42c0db4d4af2e95fd0a243efa26708508da1b2e
Maudien Smittenaar <110417@st-maartenscollege.nl>
2020-06-24 07:56:42 +0000

    woordenlijst voor het spel toegevoegd.

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
 print("We spelen het spel galgje. Leuk dat je meedoet! Raad letters en kom zo achter het woord. Weet het je hele woord? Type dan het woord voluit om te winnen!")
+
+woordenlijst = ["school", "corona", "leerwerk", "huiswerk", "vakantie", "zomer", "python","informatica", "winnaar", "computer", "schoolboek", "zon", "zwemmen", "uitrusten"]
